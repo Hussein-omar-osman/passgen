f5c0a572084bed0fc62af3168957bd6b6eb5c9a8
Hussein Omar <husseinomar6190@gmail.com>
2023-01-05 11:38:49 +0300

    guard checking params

--- lib/pass_gen_web/controllers/pass_controller.ex
+++ lib/pass_gen_web/controllers/pass_controller.ex
@@ -9,13 +9,31 @@ defmodule PassGenWeb.PassController do
 
     # conn
     # |> render("ack.json", params)
-    # params = %{"length" => "20"}
-    case PasswordGenerator.generate(params) do
-      {:ok, pass} -> json(conn, Map.merge(%{"password" => pass}, params))
+    new_params = check_params(params)
+    IO.puts("++++++++++++++++")
+    IO.inspect(params)
+    IO.puts("++++++++++++++++")
+    IO.inspect(new_params)
+    case PasswordGenerator.generate(new_params) do
+      {:ok, pass} -> json(conn, Map.merge(%{"password" => pass}, new_params))
       {:error, error} -> json(conn, %{error: error})
     end
     # PasswordGenerator.generate(params)
-    json(conn, %{password: "pass"})
+    # json(conn, %{password: "pass"})
+  end
+
+  defp check_params(params) do
+    case params === %{} do
+      true -> %{"length" => "15","numbers" => "true","symbols" => "true","uppercase" => "true"}
+      false -> check_for_length(params)
+    end
+  end
+
+  defp check_for_length(obj)  do
+    case Map.has_key?(obj, "length") do
+      true -> obj
+      false -> Map.merge(%{"length" => "15"}, obj)
+    end
   end
 
 end
