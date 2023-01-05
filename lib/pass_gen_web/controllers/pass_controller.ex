c44a1c7d21135a4329db8660d44e4575680b6268
Hussein Omar <husseinomar6190@gmail.com>
2023-01-05 11:39:36 +0300

    code clean-up

--- lib/pass_gen_web/controllers/pass_controller.ex
+++ lib/pass_gen_web/controllers/pass_controller.ex
@@ -2,24 +2,11 @@ defmodule PassGenWeb.PassController do
   use PassGenWeb, :controller
 
   def pass(conn, params) do
-    # IO.puts("++++++++++++++++")
-    # IO.inspect(conn)
-    # IO.puts("++++++++++++++++")
-    # IO.inspect(params)
-
-    # conn
-    # |> render("ack.json", params)
     new_params = check_params(params)
-    IO.puts("++++++++++++++++")
-    IO.inspect(params)
-    IO.puts("++++++++++++++++")
-    IO.inspect(new_params)
     case PasswordGenerator.generate(new_params) do
       {:ok, pass} -> json(conn, Map.merge(%{"password" => pass}, new_params))
       {:error, error} -> json(conn, %{error: error})
     end
-    # PasswordGenerator.generate(params)
-    # json(conn, %{password: "pass"})
   end
 
   defp check_params(params) do
